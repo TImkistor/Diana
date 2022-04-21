55614b884213075de1d57c68fceeb5e2459d8b1d
TImkistor <tim.kisslove@gmail.com>
2022-04-21 10:41:35 +0300

    first commit

---
+++ script/scripts10.js
@@ -0,0 +1,13 @@
+document.getElementById('input').addEventListener('change', (e)=>{
+    const input = e.currentTarget;
+    let value = input.value;
+    let scrollTop = document.getElementById('problem-st');
+if (value=="8901")
+{
+    window.location = "https://developer.mozilla.org/ru/docs/Web/API/Location"
+}
+else
+{
+    scrollTop.className = "problem-yes"
+}
+})
